f696c3dd9e7de9824e4d510f47ff0bd0cc7477cf
Aleksandr Voronovich <vsanya93@mail.ru>
2022-03-23 01:39:42 +0300

    fixed

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -9,18 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class TicketManagerTest {
     TicketRepository repository = new TicketRepository();
     TicketManager manager = new TicketManager(repository);
-    Ticket ticket1 = new Ticket(1, 13000, "LED", "GOJ",180);
-    Ticket ticket2 = new Ticket(2, 12000, "DME", "EGO",120);
-    Ticket ticket3 = new Ticket(3, 12600, "KUF", "OGZ",130);
-    Ticket ticket4 = new Ticket(4, 14200, "FRU", "RVH",190);
-    Ticket ticket5 = new Ticket(5, 15600, "NUX", "KZN",230);
-    Ticket ticket6 = new Ticket(6, 13400, "SKX", "SLY",200);
-    Ticket ticket7 = new Ticket(7, 14000, "PKV", "PEZ",320);
-    Ticket ticket8 = new Ticket(8, 11300, "NUX", "KZN",150);
-    Ticket ticket9 = new Ticket(9, 18000, "VOZ", "VOG",110);
-    Ticket ticket10 = new Ticket(10, 16000, "KZN", "URS",280);
-    Ticket ticket11 = new Ticket(11, 10000, "LPK", "MMK",190);
-    Ticket ticket12 = new Ticket(12, 9000, "NYM", "AER",140);
+    Ticket ticket1 = new Ticket(1, 13000, "LED", "GOJ", 180);
+    Ticket ticket2 = new Ticket(2, 12000, "DME", "EGO", 120);
+    Ticket ticket3 = new Ticket(3, 12600, "KUF", "OGZ", 130);
+    Ticket ticket4 = new Ticket(4, 14200, "LED", "GOR", 190);
+    Ticket ticket5 = new Ticket(5, 15600, "NUX", "KZN", 230);
+    Ticket ticket6 = new Ticket(6, 13400, "SKX", "SLY", 200);
+    Ticket ticket7 = new Ticket(7, 14000, "PKV", "PEZ", 320);
+    Ticket ticket8 = new Ticket(8, 11300, "NUX", "KZN", 150);
+    Ticket ticket9 = new Ticket(9, 18000, "VOZ", "VOG", 110);
+    Ticket ticket10 = new Ticket(10, 16000, "KZN", "URS", 280);
+    Ticket ticket11 = new Ticket(11, 10000, "LPK", "MMK", 190);
+    Ticket ticket12 = new Ticket(12, 9000, "NYM", "AER", 140);
 
 
     @Test
@@ -43,7 +43,7 @@ class TicketManagerTest {
     }
 
     @Test
-    void shouldFindAirportAndSort() {
+    void shouldFindAirportWhenTwoMatch() {
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
@@ -58,6 +58,78 @@ class TicketManagerTest {
         manager.add(ticket12);
         Ticket[] expected = new Ticket[]{ticket8, ticket5};
         Ticket[] actual = manager.findByAirport("NUX", "KZN");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindAll() {
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+        manager.add(ticket10);
+        manager.add(ticket11);
+        manager.add(ticket12);
+        Ticket[] expected = new Ticket[]{ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8, ticket9, ticket10, ticket11, ticket12};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindNonExisting() {
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+        manager.add(ticket10);
+        manager.add(ticket11);
+        manager.add(ticket12);
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findByAirport("PKV", "EGO");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNonFound() {
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+        manager.add(ticket10);
+        manager.add(ticket11);
+        manager.add(ticket12);
+        Ticket[] expected = new Ticket[0];
+        Ticket[] actual = manager.findByAirport("NOZ", "GIN");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCheckWhenNoTickets() {
+        Ticket[] expected = {};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindWhenOneTicket() {
+        manager.add(ticket5);
+        Ticket[] expected = new Ticket[]{ticket5};
+        Ticket[] actual = repository.findAll();
         assertArrayEquals(expected, actual);
     }
 }
--- src/test/java/ru/netology/repository/TicketRepositoryTest.java
+++ src/test/java/ru/netology/repository/TicketRepositoryTest.java
@@ -9,18 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TicketRepositoryTest {
     TicketRepository repository = new TicketRepository();
-    Ticket ticket1 = new Ticket(1, 13000, "LED", "GOJ",180);
-    Ticket ticket2 = new Ticket(2, 12000, "DME", "EGO",120);
-    Ticket ticket3 = new Ticket(3, 12600, "KUF", "OGZ",130);
-    Ticket ticket4 = new Ticket(4, 14200, "FRU", "RVH",190);
-    Ticket ticket5 = new Ticket(5, 15600, "NUX", "KZN",230);
-    Ticket ticket6 = new Ticket(6, 13400, "SKX", "SLY",200);
-    Ticket ticket7 = new Ticket(7, 14000, "PKV", "PEZ",320);
-    Ticket ticket8 = new Ticket(8, 11300, "NUX", "KZN",150);
-    Ticket ticket9 = new Ticket(9, 18000, "VOZ", "VOG",110);
-    Ticket ticket10 = new Ticket(10, 16000, "KZN", "URS",280);
-    Ticket ticket11 = new Ticket(11, 10000, "LPK", "MMK",190);
-    Ticket ticket12 = new Ticket(12, 9000, "NYM", "AER",140);
+    Ticket ticket1 = new Ticket(1, 13000, "LED", "GOJ", 180);
+    Ticket ticket2 = new Ticket(2, 12000, "DME", "EGO", 120);
+    Ticket ticket3 = new Ticket(3, 12600, "KUF", "OGZ", 130);
+    Ticket ticket4 = new Ticket(4, 14200, "FRU", "RVH", 190);
+    Ticket ticket5 = new Ticket(5, 15600, "NUX", "KZN", 230);
+    Ticket ticket6 = new Ticket(6, 13400, "SKX", "SLY", 200);
+    Ticket ticket7 = new Ticket(7, 14000, "PKV", "PEZ", 320);
+    Ticket ticket8 = new Ticket(8, 11300, "NUX", "KZN", 150);
+    Ticket ticket9 = new Ticket(9, 18000, "VOZ", "VOG", 110);
+    Ticket ticket10 = new Ticket(10, 16000, "KZN", "URS", 280);
+    Ticket ticket11 = new Ticket(11, 10000, "LPK", "MMK", 190);
+    Ticket ticket12 = new Ticket(12, 9000, "NYM", "AER", 140);
 
     @Test
     void shouldRemoveById() {
@@ -43,22 +43,88 @@ class TicketRepositoryTest {
     }
 
     @Test
+    void shouldRemoveByIdWhereOneTicket() {
+        repository.save(ticket1);
+        repository.removeById(1);
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRemoveByIdWhereTwoTickets() {
+        repository.save(ticket1);
+        repository.save(ticket2);
+        repository.removeById(1);
+        Ticket[] expected = new Ticket[]{ticket2};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRemoveFewID() {
+        repository.save(ticket1);
+        repository.save(ticket2);
+        repository.save(ticket3);
+        repository.save(ticket4);
+        repository.removeById(1);
+        repository.removeById(3);
+        Ticket[] expected = new Ticket[]{ticket2, ticket4};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     void shouldSortByPrice() {
-    repository.save(ticket1);
-    repository.save(ticket2);
-    repository.save(ticket3);
-    repository.save(ticket4);
-    repository.save(ticket5);
-    repository.save(ticket6);
-    repository.save(ticket7);
-    repository.save(ticket8);
-    repository.save(ticket9);
-    repository.save(ticket10);
-    repository.save(ticket11);
-    repository.save(ticket12);
-    Ticket[] expected =  new Ticket[]{ticket12, ticket11, ticket8, ticket2, ticket3, ticket1, ticket6, ticket7, ticket4, ticket5, ticket10, ticket9};
-    Ticket[] actual = repository.findAll();
-    Arrays.sort(actual);
-    assertArrayEquals(expected,actual);
+        repository.save(ticket7);
+        repository.save(ticket12);
+        repository.save(ticket2);
+        repository.save(ticket8);
+        repository.save(ticket1);
+        repository.save(ticket9);
+        repository.save(ticket3);
+        repository.save(ticket11);
+        repository.save(ticket6);
+        repository.save(ticket5);
+        repository.save(ticket10);
+        repository.save(ticket4);
+        Ticket[] expected = new Ticket[]{ticket12, ticket11, ticket8, ticket2, ticket3, ticket1, ticket6, ticket7, ticket4, ticket5, ticket10, ticket9};
+        Ticket[] actual = repository.findAll();
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCheckWhenNoTickets() {
+        repository.findAll();
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSaveOneTicket() {
+        repository.save(ticket1);
+        Ticket[] expected = new Ticket[]{ticket1};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSaveTwoTickets() {
+        repository.save(ticket1);
+        repository.save(ticket2);
+        Ticket[] expected = new Ticket[]{ticket1, ticket2};
+        Ticket[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSortWhenOneElement() {
+        repository.save(ticket7);
+        Ticket[] expected = new Ticket[]{ticket7};
+        Ticket[] actual = repository.findAll();
+        Arrays.sort(actual);
+        assertArrayEquals(expected, actual);
     }
 }
